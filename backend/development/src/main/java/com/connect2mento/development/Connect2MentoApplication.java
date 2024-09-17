769d45204cc4cd836dd1beaa59de735a4edc6a1c
YuKi <yukifernando0001@gmail.com>
2024-09-17 17:17:39 +0530

    [init] Backend - Springboot

---
+++ backend/development/src/main/java/com/connect2mento/development/Connect2MentoApplication.java
@@ -0,0 +1,13 @@
+package com.connect2mento.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Connect2MentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Connect2MentoApplication.class, args);
+	}
+
+}
