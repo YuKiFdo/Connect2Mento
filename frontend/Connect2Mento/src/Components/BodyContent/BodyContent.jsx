5eba6957544f42a38c8ae8235bf039501cf28cee
LishaniChamathka <162943365+LishaniChamathka@users.noreply.github.com>
2024-09-22 22:32:48 +0530

    body section change typing

--- frontend/Connect2Mento/src/Components/BodyContent/BodyContent.jsx
+++ frontend/Connect2Mento/src/Components/BodyContent/BodyContent.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './BodyContent.css'
 import ImageContent from '../ImageContent/ImageContent'
+import Typing from 'react-typing-effect'
 
 
 function BodyContent() {
+
+  // const typewriterRef = useRef(null);
+
+  // const {text} = useTypewriter({
+  //   ref: typewriterRef,
+  //   words: ['Mentor With ', 'Vedio Conversations', 'Text Conversations', 'Library Facilities', 'Experience'],
+  //   loop: true,
+  //   typeSpeed:120,
+  //   deleteSpeed: 80,
+  // });
+
+  // console.log('Text being typed:', text);
+
   return (
     <section className='hero'>
 
     <div className="left-section">
 
       <h1 id="heading">
-        
-      <span class="word">Find </span> 
-      <span class="word">the <br/></span> 
-      <span class="word">Best <br/></span>
-      <span class="word">Mentor </span> 
-      <span class="word">With <br/></span> 
-      <span class="word">Us</span>
+        <span class="word">Find the <br/>  Best <br/> </span> 
+        <span>
+        <Typing 
+            text={['Mentor With', 'Virtual Meeting', 'Text Exchange', 'Library Spaces', 'Experience']}
+            speed={100}
+            eraseSpeed={50}
+            eraseDelay={2000}
+            typingDelay={500}
+            className="typing-text"
+          />
+        </span>
+        <span class="word"><br/> With Us </span> 
       </h1>
 
       <div className='imgContainer'>
