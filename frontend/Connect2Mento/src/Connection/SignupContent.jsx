2123f2fcf32d90d6f5e7feadacc0dab90ac77ca8
LishaniChamathka <162943365+LishaniChamathka@users.noreply.github.com>
2024-09-23 15:48:04 +0530

    login and sign up new changes

--- frontend/Connect2Mento/src/Connection/SignupContent.jsx
+++ frontend/Connect2Mento/src/Connection/SignupContent.jsx
@@ -1,137 +1,55 @@
 import React from 'react'
 import HeaderContent from '../Components/HeaderContent/HeaderContent'
 import './SignupContent.css'
+import logoimage from '../../Images/iconc2m.png'
 
 
-const SignupContent = () => {
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const togglePasswordVisibility = () => {
-      setPasswordVisible(!passwordVisible);
-  };
-  
+function SignupContent() {
   return (
 
     <div id="wrapper">
         <HeaderContent/> 
 
-    {/* <div class="back">
-            <div class="blok">
-              <span class="signtitle">SignUp</span>
-            </div>
-          
-            <div class="firstnamefield">
-                <p class="firstnametitle">First Name</p>
-                <div className="searchbar">
-                  <input 
-                    type="text"  
-                    className="firstname"
-                  />
-                </div>
-            </div>
-
-            <div class="lastnamefield">
-                <p class="lastnametitle">Last Name</p>
-                <div className="searchbar">
-                  <input 
-                    type="text"  
-                    className="lastname"
-                  />
-                </div>
-            </div>
-            
-            <div class="emailfield">
-                <p class="emailtitle">Email Account</p>
-                <div className="searchbar">
-                  <input 
-                    type="text"  
-                    className="email"
-                  />
-                </div>
-            </div>
-            
-            <div class="paswordfield">
-                <p class="passwordtitle">Password</p>
-                <div className="searchbar">
-                  <input 
-                    type="text"  
-                    className="password"
-                  />
-                </div>
-            </div>
-            
-            <div class='signupbutton'>
-            <button className="signupbtn">Sign Up</button>
-            </div>
-            
-        
-            
-            <div class="name"></div>
-            
-            <div class="signupwithtitle">
-                <p class="signupwith">Or Sign Up With</p>
-            </div>
-            
-            <span class="alreadyaccounnt">Already have an account ?</span>
-
-            <a href="/login" className="loginlink">Login</a>
-            
-            <div class="googleicon"></div>
-            <span class="googleword">Google</span>
-            
-            <span class="microsoftword">Microsoft</span>
-            
-            <div class="microsofticon"></div>
-        
-        </div> */}
-
-
-
-
-
             <div className="signup-container">
             <div className="signup-left">
                 <div className="logo">
-                    <img src="/logo.png" alt="Logo" />
+                  <img src={logoimage} class="mainlogo" alt="logo" />
                 </div>
             </div>
             <div className="signup-right">
                 <h2>Sign up as a mentee</h2>
                 <form className="signup-form">
-                    <label>First name</label>
+                    <label className="first">First name</label>
                     <input type="text" placeholder="First name" />
 
-                    <label>Last name</label>
+                    <label className="last">Last name</label>
                     <input type="text" placeholder="Last name" />
 
-                    <label>Email</label>
+                    <label className="emailtitle">Email</label>
                     <input type="email" placeholder="Email" />
 
-                    <label>Password</label>
+                    <label className="password">Password</label>
                     <div className="password-field">
                         <input 
-                            type={passwordVisible ? "text" : "password"} 
+                            type= "password"
                             placeholder="Password" 
                         />
-                        <span className="show-password" onClick={togglePasswordVisibility}>
-                            {passwordVisible ? 'Hide' : 'Show'}
-                        </span>
-                    </div>
-
-                    <ul className="password-rules">
-                        <li>Must be at least 8 characters</li>
-                        <li>Must include one lowercase character</li>
-                        <li>Must include one uppercase character</li>
-                        <li>Can't be too common</li>
-                    </ul>
-
-                    <div className="captcha">
-                        <div className="captcha-placeholder">
-                            {/* Replace this with actual reCAPTCHA integration */}
-                            <p>I'm not a robot</p>
-                        </div>
                     </div>
 
                     <button className="signup-button">Sign up</button>
+
+                    <div className="or-divider">Or</div>
+                    <button className="google-login">Log in with Google</button>
+
+
+                    
+
+                    <div className='footerloginlink'>
+                      <span class="alreadyaccount">Already have an account ? </span>
+                      <a href="/login" className="loginmentee">Login</a>
+                    </div>
+                    
+                    
                 </form>
             </div>
         </div>
